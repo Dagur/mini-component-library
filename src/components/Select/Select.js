1b6f802ba19519ab6a4d49dad0ae935dbb892141
Dagur Páll Ammendrup <dagurp@gmail.com>
2023-01-24 22:51:14 +0000

    Couldn't figure out the changing width

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
-
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import Icon from "../Icon";
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../constants";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Button>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <StyledIcon size={24} strokeWidth={2} id="chevron-down" />
+    </Button>
   );
 };
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 10px;
+  right: 12px;
+  color: currentColor;
+`;
+
+const Button = styled.div`
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  width: fit-content;
+  position: relative;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const StyledSelect = styled.select`
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  appearance: none;
+  border: none;
+  font-size: 1rem;
+  color: currentColor;
+
+  &:focus-visible {
+    outline: 2px solid ${COLORS.primary};
+    outline-radius: 8px;
+  }
+`;
 
 export default Select;
